3d5a2813ba69aa9611904e52566503acfeda4f04
Gustaviinhoow <gustavorodrigues_123@hotmail.com>
2019-11-25 22:23:01 -0300

    att php old

--- API_Conexao_Android-master/php/processaandroid.php
+++ API_Conexao_Android-master/php/processaandroid.php
@@ -5,13 +5,31 @@ $usuario = "crm_admin";
 $senha = "senai";
 $banco = "crm";
 	
+/*
+$pdo = new PDO("mysql:host=$servidor;dbname=$banco", "$usuario", "$senha"); 
+$pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+$conexao = mysqli_connect($servidor, $usuario,
+	$senha, $banco);
+if(!$conexao){
+	die(mysqli_error($conexao));
+}*/
+//se o comando foi enviado...
 $comando = verificaValorPost("comando");
 if($comando === "teste"){
+	// se o valor foi setado
 	$valor = verificaValorPost('valor');
 	$valorr = (floatval($valor))* 2;
+	//enviar duplicado de volta para o android
 	echo("$valorr");
 }
 else if($comando == 'inserir'){
+	/*$resultado = mysqli_query($conexao, 
+		"INSERT INTO clientes 
+		(nome,cpf,sexo,endereco,cidade,estado,telefone)
+		VALUES (\"$nome\", \"$cpf\",\"$sexo\",
+			\"$endereco\", \"$cidade\",\"$estado\",
+			\"$telefone\") ");*/
+			//echo($_POST["nome"]);
 	$cpf = verificaValorPost('cpf');
 	$nome = verificaValorPost('nome');
 	$sobrenome = verificaValorPost('sobrenome');
@@ -40,6 +58,7 @@ else if($comando == 'inserir'){
 				echo(" Algum campo excedeu o limite de caracteres");
 			break;
 		}
+		//throw $e->getCode();
 	}
 }
 
@@ -49,6 +68,7 @@ function verificaValorPost($var){
 	}
 	return $_POST["$var"];
 }
+//================== COMANDO SELECT =======================
 if ($comando == 'buscarCpf' || $isDebug) {
 	if($isDebug){}
 	$resultado = mysqli_query($conexao, 
@@ -56,6 +76,7 @@ if ($comando == 'buscarCpf' || $isDebug) {
 	if($resultado){
 		$linha = mysqli_fetch_array($resultado);
 		$registros = array();
+		//extraindo os dados do bd
 		$linha2 = array(			
 			'cpf'=>$linha['cpf'],
 			'nome'=>$linha['nome'], 
@@ -81,15 +102,18 @@ elseif ($comando == 'buscarNome' || $isDebug) {
 	}
 }
 if($resultado){
-							
+							//B 				C
 	array_push($jsonArray, array('resultado'=>'OK'));
 }else{
 	array_push($jsonArray, array('resultado'=>'NOK'));
 }
-						
+						//A
 echo json_encode(array($comando=>$jsonArray));
 
 echo("$nome, $cpf, $sobrenome");
+
+
+//================== COMANDO INSERT =======================
 
 if($operacao == "Inserir"){
 	
@@ -105,3 +129,174 @@ if($operacao == "Inserir"){
 		mysqli_error($conexao);
 	}	
 }
+
+//===============================================
+
+/*
+$isDebug = false;
+$jsonArray = array();
+if(isset($_POST['teste']) || $isDebug){
+	if($isDebug) $_POST['teste'] = "debug";
+	array_push($jsonArray, 
+		array('resultado'=>$_POST['teste'].''.$_POST['teste']));
+	//equivalente à $jsonArray['resultado'] = 'OK';
+	echo json_encode(array('teste'=>$jsonArray));
+}
+
+//verifica se a chave do $_POST está preenchida ou não
+if(isset($_POST['comando']))$comando = $_POST['comando'];
+if(isset($_POST['nome']))$nome = $_POST['nome'];
+else $nome = '';
+if(isset($_POST['cpf']))$cpf = $_POST['cpf'];
+else $cpf = '';
+if(isset($_POST['sexo']))$sexo = $_POST['sexo'];
+else $sexo = '';
+if(isset($_POST['endereco']))$endereco = $_POST['endereco'];
+else $endereco = '';
+if(isset($_POST['cidade']))$cidade = $_POST['cidade'];
+else $cidade = '';
+if(isset($_POST['estado']))$estado = $_POST['estado'];
+else $estado = '';
+if(isset($_POST['telefone']))$telefone = $_POST['telefone'];
+else $telefone = '';
+
+if($comando == 'inserir' || $isDebug){
+	if($isDebug){}
+	$resultado = mysqli_query($conexao, 
+		"INSERT INTO clientes 
+		(nome,cpf,sexo,endereco,cidade,estado,telefone)
+		VALUES (\"$nome\", \"$cpf\",\"$sexo\",
+			\"$endereco\", \"$cidade\",\"$estado\",
+			\"$telefone\") ");
+}elseif($comando == 'alterar' || $isDebug){
+	if($isDebug){}
+	$resultado = mysqli_query($conexao, 
+		"UPDATE clientes 
+		SET nome=\"$nome\", sexo=\"$sexo\",
+			endereco=\"$endereco\", cidade=\"$cidade\",
+			estado=\"$estado\", telefone=\"$telefone\"
+		WHERE cpf=\"$cpf\"; ");
+}elseif($comando == 'excluir' || $isDebug){
+	if($isDebug){}
+	$resultado = mysqli_query($conexao, 
+		"DELETE FROM clientes WHERE cpf=\"$cpf\"; ");
+}
+elseif ($comando == 'buscarCpf' || $isDebug) {
+	if($isDebug){}
+	$resultado = mysqli_query($conexao, 
+		"SELECT * FROM clientes WHERE cpf=\"$cpf\" ;");
+	if($resultado){
+		$linha = mysqli_fetch_array($resultado);
+		$registros = array();
+		//extraindo os dados do bd
+		$linha2 = array(
+			'nome'=>$linha['nome'], 
+			'cpf'=>$linha['cpf'],
+			'sexo' =>$linha['sexo'],
+			'endereco' =>$linha['endereco'],
+			'cidade'=>$linha['cidade'],
+			'estado'=>$linha['estado'],
+			'telefone'=>$linha['telefone']);
+		array_push($registros, $linha2);
+		array_push($jsonArray, array('registros'=>$registros));
+	}
+}
+elseif ($comando == 'buscarNome' || $isDebug) {
+	if($isDebug){}
+	$resultado = mysqli_query($conexao, 
+		"SELECT * FROM clientes WHERE nome LIKE \"%$nome%\" ;");
+	if($resultado){
+		$registros = array();
+		while($linha = mysqli_fetch_array($resultado)){
+			$linha2 = array(
+			'nome'=>$linha['nome'], 
+			'cpf'=>$linha['cpf'],
+			'sexo' =>$linha['sexo'],
+			'endereco' =>$linha['endereco'],
+			'cidade'=>$linha['cidade'],
+			'estado'=>$linha['estado'],
+			'telefone'=>$linha['telefone']);
+			array_push($registros, $linha2);
+		}
+		array_push($jsonArray, array('registros'=>$registros));
+	}
+}
+if($resultado){
+							//B 				C
+	array_push($jsonArray, array('resultado'=>'OK'));
+}else{
+	array_push($jsonArray, array('resultado'=>'NOK'));
+}
+						//A
+echo json_encode(array($comando=>$jsonArray));
+/*
+//echo("$nome, $cpf, $sexo, $endereco, $cidade, $estado, $telefone, $operacao");
+
+if($sexo == "masculino") $sexo = "M";
+else $sexo = "F";
+
+
+if($operacao == "Inserir"){
+	
+
+}elseif($operacao == "Alterar"){
+	$resultado = mysqli_query($conexao, 
+		"UPDATE clientes 
+		SET nome=\"$nome\", sexo=\"$sexo\",
+			endereco=\"$endereco\", cidade=\"$cidade\",
+			estado=\"$estado\", telefone=\"$telefone\"
+		WHERE cpf=\"$cpf\"; ");
+	if($resultado){
+		echo("<br>Dados atualizados com sucesso!");
+	}else{
+		echo("<br>Os dados não puderam ser atualizados. Erro:");
+		mysqli_error($conexao);
+	}
+}elseif($operacao == "Excluir"){
+	$resultado = mysqli_query($conexao,
+		"DELETE FROM clientes WHERE cpf=\"$cpf\"; ");
+	if($resultado) echo("<br>Dados deletados com sucesso!");
+	else echo("<br>Os dados não puderam ser excluídos.");
+}
+elseif($operacao == "Buscar por CPF"){
+	$resultado = mysqli_query($conexao,
+		"SELECT * FROM clientes WHERE cpf=\"$cpf\" ;");
+	while($linha = mysqli_fetch_array($resultado)){
+		echo(''.$linha['nome'].
+			' '.$linha['cpf'].
+			' '.$linha['telefone']);
+		//header("Location: index.php");
+	}
+}else if($operacao == "Buscar por nome"){
+	$resultado = mysqli_query($conexao,
+		"SELECT * FROM clientes WHERE nome LIKE \"%$nome%\" ;");
+	if($resultado){
+		echo("<table style=\"border:1px solid black\">
+				<tr>
+				<th>Nome</th>
+				<th>CPF</th>
+				<th>Sexo</th>
+				<th>Endereço</th>
+				<th>Cidade</th>
+				<th>Estado</th>
+				<th>Telefone</th>
+				</tr>");
+		while($linha = mysqli_fetch_array($resultado)){
+			echo("<tr>
+				<td>".$linha['nome']."</td>
+				<td>".$linha['cpf']."</td>
+				<td>".$linha['sexo']."</td>
+				<td>".$linha['endereco']."</td>
+				<td>".$linha['cidade']."</td>
+				<td>".$linha['estado']."</td>
+				<td>".$linha['telefone']."</td>
+				</tr>");
+		}
+		echo("</table>");
+	}else{
+		echo("<br>Os dados não puderam ser buscados.");
+	}
+	
+}
+*/
+?>
